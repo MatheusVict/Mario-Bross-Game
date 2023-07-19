9a7cf36db18227cee1de98a505e901b348aaa6d5
MatheusVict <matheusvictorhenrique@gmail.com>
2023-07-18 21:01:19 -0300

    main layout

--- app/src/main/java/dev/matheusvict/mariogame/MainActivity.kt
+++ app/src/main/java/dev/matheusvict/mariogame/MainActivity.kt
@@ -1,7 +1,9 @@
 package dev.matheusvict.mariogame
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import dev.matheusvict.mariogame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
     }
 }
